55589175d9c74305c7343d32748427ffb82fed84
amirk85 <khanamir8589@gmail.com>
2026-03-18 13:01:58 +0530

    init

--- nvim/.config/nvim/lua/amir/plugins/ui.lua
+++ nvim/.config/nvim/lua/amir/plugins/ui.lua
@@ -3,7 +3,7 @@ return {
   {
     "NvChad/nvim-colorizer.lua",
     event = { "BufReadPre", "BufNewFile" },
-    opts = { user_default_options = { tailwind = true } },
+    opts = { user_default_options = { tailwind = true, css = true, css_fn = true } },
   },
 
   -- lualine
